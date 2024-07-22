922169178a860d81c766c05b7210f59766f16bc9
Mehdi Zymberi <zymberi.mz@gmail.com>
2024-07-22 12:20:15 +0200

    added state between component <NoProjectSelect/> & <ProjectSidebar/>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+
+import { useState } from "react";
+import NoProjectSelected from "./component/NoProjectSelected";
+import ProjectSideBar from "./component/ProjectSideBar";
+import NewProject from "./component/NewProject";
+
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSideBar onStartAddProject={handleStartAddProject} />
+      {content}
+    </main>
   );
 }
 
